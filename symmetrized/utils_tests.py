5f6dfc16d5e765bb57eb20b070a89d036962c8df
Stach <staho.kazmierowski@gmail.com>
2021-03-04 17:45:45 +0100

    first tests of utils

--- symmetrized/utils_tests.py
+++ symmetrized/utils_tests.py
@@ -1,8 +1,29 @@
 import unittest
+from utils import divides, next_divide, symmetrized_pure_payoff_a
+import numpy as np
+
+
+
+
+# TODO trzeba zrobić testy
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_divides_generating(self):
+        for i in range(2, 10):
+            self.assertTrue((next_divide(divides(20, i)) ==  divides(21, i)).all())
+
+    def test_symmetrized_payoff(self):
+        symmetrized_strategies_A = divides(7,3)
+        symmetrized_strategies_B = divides(10,3)
+        for i in range(symmetrized_strategies_A.shape[1]):
+            strategy_A = symmetrized_strategies_A[:,i]
+            for j in range(symmetrized_strategies_B.shape[1]):
+                strategy_B = symmetrized_strategies_A[:,i]
+                self.assertEqual(symmetrized_pure_payoff_a(strategy_A, strategy_B),
+                                 -symmetrized_pure_payoff_a(strategy_B, strategy_A))
+
 
 if __name__ == '__main__':
     unittest.main()
+
+
