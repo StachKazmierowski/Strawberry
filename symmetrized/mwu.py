0357503318e38aacd0a6c144cdd660634d151959
Stach <staho.kazmierowski@gmail.com>
2021-03-17 20:32:00 +0100

    first ~working version of mwu

---
+++ symmetrized/mwu.py
@@ -0,0 +1,42 @@
+from utils import payoff_matrix, pd_payoff_matrix, divides
+import numpy as np
+import pandas as pd
+np.set_printoptions(suppress=True)
+
+def MWU_symmetric_game_algorithm_working(resources_number, fields_number, phi=1/2, steps_number=1000):
+    try:
+        path = "./data/payoff_matrices/payoff_matrix(" + str(resources_number) + \
+               "," + str(resources_number) + "," + str(fields_number) + ").csv"
+        payoff_mat = np.delete(pd.read_csv(path).to_numpy(), 0,1)
+    except:
+        print("Loaded failed")
+        payoff_mat = payoff_matrix(resources_number, resources_number, fields_number)
+    payoff_mat *= -1
+    # print(payoff_mat)
+    rows_number = payoff_mat.shape[0]
+    cols_number = payoff_mat.shape[1]
+
+    p_0 = np.ones((1, rows_number))
+    p_0 = p_0/rows_number
+    p_t = p_0
+    j_sumed = np.zeros((cols_number, 1))
+    smallest_column_payoff = 1
+    t_smallest_column_payoff = 0
+    p_best = p_0
+    p_t_sum = np.zeros((1, rows_number))
+    for i in range (steps_number):
+        payoffs = np.matmul(p_t, payoff_mat)
+        j_best_response = np.argmax(payoffs)
+        if(payoffs[0, j_best_response] < smallest_column_payoff):
+            t_smallest_column_payoff = i
+            smallest_column_payoff = payoffs[0, j_best_response]
+            p_best = p_t
+        j_sumed[j_best_response] += 1
+        m_t = payoff_mat[:,j_best_response]
+        p_t = np.multiply((1 - phi * m_t), p_t)
+        p_t = p_t/p_t.sum()
+        p_t_sum = p_t_sum + p_t
+    return p_best, j_sumed/j_sumed.sum()
+res = MWU_symmetric_game_algorithm_working(6,5,1/2,5000)
+# print(np.matmul(payoff_matrix(6,6,5), res))
+print(res[0].reshape(res[0].shape[1], res[0].shape[0]), "\n", "\n", res[1])
