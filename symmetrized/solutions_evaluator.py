3c99bd5023e7b1edf6a2f0b6e297b8eb018d2b8f
Stach <staho.kazmierowski@gmail.com>
2021-03-21 16:08:21 +0100

    file swap after mergin problems

--- symmetrized/solutions_evaluator.py
+++ symmetrized/solutions_evaluator.py
@@ -15,8 +15,11 @@ def eval_symmetric_solution(resource_number, fields_number, phi, steps_number):
     payoff_mat = try_reading_symmetric_matrix_numpy(resource_number, fields_number)
     row_strategy = chopstic_row_solution_to_vector(6,5,parse_file(path, filename))
     # print(row_strategy)
-    # print(np.mu(np.matmul(row_strategy, payoff_mat), (column_player_strategy>0))
-    return 0
+    print(np.matmul(np.matmul(row_strategy, payoff_mat), column_player_strategy))
+    return
+
+
+
 
 def chopstic_row_solution_to_vector(resource_number, fields_number, solution):
     matrix_pandas = try_reading_symmetric_matrix(resource_number, fields_number)
@@ -28,25 +31,6 @@ def chopstic_row_solution_to_vector(resource_number, fields_number, solution):
                 strategy[0,i] = solution.iloc[j][0]
     return strategy
 
-def eval_strategy(payoff_matrix, row_solution, column_solution, strategy, game_value=0):
-    row_player_strategy = algoritmic_strategy[0]
-    column_player_strategy = algoritmic_strategy[1]
-    column_solution = column_solution.reshape(column_solution.shape[1], 1)
-    row_vector = np.matmul(row_player_strategy, payoff_matrix)
-    print(payoff_matrix.shape)
-    print(np.multiply(row_vector.reshape(row_vector.shape[1], row_vector.shape[0]), column_solution>0))
-    # print(row_solution>0)
-    # print(column_solution>0)
-    return 0
-
-# Mock data
-(resource_number, fields_number, phi, steps_number) = (6,5,1/2,100)
-algoritmic_strategy = MWU_symmetric_game_algorithm(resource_number, fields_number, phi, steps_number)
-payoff_mat = try_reading_symmetric_matrix_numpy(resource_number, fields_number)
-print(payoff_mat.shape)
-solution_A = chopstic_row_solution_to_vector(6,5,parse_file(path, filename))
-print(eval_strategy(payoff_mat, solution_A, solution_A, algoritmic_strategy))
-
 # chopstic_row_solution_to_vector(6,5,parse_file(path, filename))
-# eval_symmetric_solution(6,5,1/2, 1000)
+eval_symmetric_solution(6,5,1/2, 1000)
 
