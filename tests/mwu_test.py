777bfe003c2b6e726128069d59e9b4b6e18e5b68
Stach <staho.kazmierowski@gmail.com>
2021-03-21 21:10:22 +0100

    adding test class

---
+++ tests/mwu_test.py
@@ -0,0 +1,15 @@
+from symmetrized.solutions_evaluator import eval_strategy
+from symmetrized.mwu import MWU_symmetric_game_algorithm
+from symmetrized.utils import try_reading_symmetric_matrix_numpy
+import time
+
+phis = [1/i for i in range(1, 11)]
+print(phis)
+
+
+
+
+
+
+
+
