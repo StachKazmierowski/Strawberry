818a873f852041b1303f668e8b891e0b9a85a7bb
Stach <staho.kazmierowski@gmail.com>
2021-03-21 16:50:46 +0100

    first working evaluator, based on maximum metric

--- symmetrized/solutions_evaluator.py
+++ symmetrized/solutions_evaluator.py
@@ -28,22 +28,24 @@ def chopstic_row_solution_to_vector(resource_number, fields_number, solution):
                 strategy[0,i] = solution.iloc[j][0]
     return strategy
 
-def eval_strategy(payoff_matrix, row_solution, column_solution, strategy, game_value=0):
+def eval_strategy(payoff_matrix, row_solution, column_solution, algoritmic_strategy, game_value=0):
     row_player_strategy = algoritmic_strategy[0]
     column_player_strategy = algoritmic_strategy[1]
     column_solution = column_solution.reshape(column_solution.shape[1], 1)
     row_vector = np.matmul(row_player_strategy, payoff_matrix)
-    print(payoff_matrix.shape)
-    print(np.multiply(row_vector.reshape(row_vector.shape[1], row_vector.shape[0]), column_solution>0))
-    # print(row_solution>0)
-    # print(column_solution>0)
-    return 0
+    column_vector = np.matmul(payoff_matrix, column_player_strategy)
+    print(abs(game_value - np.multiply(row_vector.reshape(row_vector.shape[1], row_vector.shape[0]), column_solution>0)))
+    print(abs(game_value - np.multiply(row_solution > 0, column_vector.reshape(column_vector.shape[1], column_vector.shape[0]))))
+    column_biggest_error = np.max(abs(game_value - np.multiply(row_vector.reshape(row_vector.shape[1], row_vector.shape[0]), column_solution>0)))
+    row_biggest_error = np.max(abs(game_value - np.multiply(row_solution > 0, column_vector.reshape(column_vector.shape[1], column_vector.shape[0]))))
+
+    return max(column_biggest_error, row_biggest_error)
 
 # Mock data
-(resource_number, fields_number, phi, steps_number) = (6,5,1/2,100)
+(resource_number, fields_number, phi, steps_number) = (6,5,1/4,1000)
 algoritmic_strategy = MWU_symmetric_game_algorithm(resource_number, fields_number, phi, steps_number)
 payoff_mat = try_reading_symmetric_matrix_numpy(resource_number, fields_number)
-print(payoff_mat.shape)
+# print(payoff_mat.shape)
 solution_A = chopstic_row_solution_to_vector(6,5,parse_file(path, filename))
 print(eval_strategy(payoff_mat, solution_A, solution_A, algoritmic_strategy))
 
