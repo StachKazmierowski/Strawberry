01ba467d1ec9684d9755f8f5cc44a8ea96ff1b30
stach <staho.kazmierowski@gmail.com>
2021-04-08 09:17:17 +0200

    finding marginal distribution function implementation

--- symmetrized/solutions_evaluator.py
+++ symmetrized/solutions_evaluator.py
@@ -26,6 +26,25 @@ def eval_strategy(payoff_matrix, row_solution, column_solution, algoritmic_strat
     row_biggest_error = np.max(abs(game_value - np.multiply(row_solution > 0, column_vector.reshape(column_vector.shape[1], column_vector.shape[0])))[row_solution > 0])
     return column_biggest_error, row_biggest_error
 
+def find_marginal_distribution(resource_number, fields_number, strategy):
+    if(strategy.shape[0] == 1):
+        strategy = strategy.reshape((strategy.shape[1], 1))
+    pure_strategies = divides(resource_number, fields_number)
+    print(pure_strategies.shape)
+    print(strategy.shape)
+    res = np.zeros((resource_number+1))
+    for i in range(pure_strategies.shape[0]):
+        if(strategy[i, 0] > 0):
+            pure_strategy = pure_strategies[i,:]
+            print(pure_strategy)
+            print(pure_strategy.shape)
+            for j in range(pure_strategies.shape[1]):
+                res[int(pure_strategy[j])] += strategy[i,0]/fields_number
+    return res
+
+mock_strategy = MWU_game_algorithm(9,9,3,1/16,100000)[1]
+print(mock_strategy)
+print(find_marginal_distribution(9,3,mock_strategy))
 # Mock data
 # (resource_number, fields_number, phi, steps_number) = (6,5,1/4,1000)
 # algoritmic_strategy = MWU_game_algorithm(resource_number, resource_number, fields_number, phi, steps_number)
