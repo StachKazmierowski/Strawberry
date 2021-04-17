fb27fbbe9e9ec7944958db972107e926ab364a71
stach <staho.kazmierowski@gmail.com>
2021-04-17 16:42:49 +0200

    working recurent version of finding H

--- symmetrized/payoff_matrix.py
+++ symmetrized/payoff_matrix.py
@@ -8,7 +8,6 @@ import math
 # Mock data
 strategy_one = divides(10,6)[0]
 strategy_two = divides(10,6)[1]
-print(strategy_one, strategy_two.shape)
 
 def single_payoff_matrix(strategy_A, strategy_B):
     assert strategy_A.shape == strategy_B.shape
@@ -19,8 +18,7 @@ def single_payoff_matrix(strategy_A, strategy_B):
         for j in range(fields_number):
             matrix[i,j] = reversed_strategy_B[j] - strategy_A[i]
     matrix = np.sign(matrix)
-    pd_matrix = pd.DataFrame(matrix, columns=reversed_strategy_B.tolist(), index=strategy_A.tolist())
-    # print(pd_matrix)
+    # pd_matrix = pd.DataFrame(matrix, columns=reversed_strategy_B.tolist(), index=strategy_A.tolist())
     return matrix
 
 def single_payoff_matrix_vectors(strategy_A, strategy_B):
@@ -28,7 +26,6 @@ def single_payoff_matrix_vectors(strategy_A, strategy_B):
     W = []
     T = []
     fields_num = matrix.shape[0]
-    # print(matrix)
     for i in range(fields_num):
         tmp_w = 0
         tmp_t = 0
@@ -69,11 +66,10 @@ def payoff(strategy_A, strategy_B):
 
 def permutations_results(strategy_A, strategy_B):
     matrix = single_payoff_matrix(strategy_A, strategy_B)
-    res = np.zeros((strategy_A.shape[0], strategy_B.shape[0]))
+    res = np.zeros((strategy_A.shape[0] + 1, strategy_B.shape[0] + 1))
     fields_number = matrix.shape[0]
     perm = list(range(fields_number))
     perm = list(set(permutations(perm)))
-    fields_number_fac = len(perm)
     k_W = 0
     k_L = 0
     for i in range(len(perm)):
@@ -86,15 +82,7 @@ def permutations_results(strategy_A, strategy_B):
         k_W = 0
         k_L = 0
     return res
-print(permutations_results(np.array([1,2,3,4]), np.array([1,2,3,4])).sum())
 
-# print(payoff(strategy_one, strategy_two))
-
-#%%
-# strategy_one = np.array([5,3])
-# strategy_two = np.array([4,4])
-
-# print(symmetrized_pure_payoff_a(strategy_one, strategy_two))
 def newton_symbol(n,k):
     return scipy.special.comb(n , k, exact=True)
 
@@ -105,9 +93,11 @@ def factorial(n):
         return math.factorial(n)
 
 def single_type_rectangle(cols_num, rows_num, rooks_num):
-    # if(cols_num == 0 or rows_num == 0):
-    #     print("ZERO in shape of single rectangle")
-    #     return 0    TODO dawało błędy jak było odifowane - chyba powinno być gdzieś wcześniej ogarnięte
+    if(cols_num < 0 or rows_num < 0 or rooks_num < 0):
+        print("UJEMNA WARTOŚĆ W PROSTOKĄCIE")
+    #     print(cols_num, rows_num, rooks_num)
+        return 0
+        ### TODO błędy przy warunku if(cols_num==0) ret 0 - może być tak, że wyrzucamy z prostokąta wszystkie wiersze i nie możemy nic w nim umieścić
     if(rooks_num > cols_num or rooks_num > rows_num):
         return 0
     if(rooks_num == 0):
@@ -121,10 +111,7 @@ def max_rook_num(W): ## TODO zajebiśćie zrobić tą funkcje bo tu wychodzi wi�
     if(len(W) == 1):
         return 1
     return 1 + max_rook_num(W - 1)
-print(max_rook_num(np.array([1,3,3])))
-# A = np.array([-1,-2,3,4,0])
-# print(np.delete(A, A <= 0))
-#%%
+
 def L_vector(W, T, fields_num):
     return - T + fields_num
 
@@ -135,46 +122,60 @@ def width_to_remove(W):
     width = 1
     full_width = W.shape[0]
     for i in range(full_width-1):
-        if(W[-i] == W[-1 - i]):
+        if(W[-i - 1] == W[-i - 2]):
             width += 1
         else:
             break
     return width
+W = np.array([0,3,3])
+print(W[-2])
+print(width_to_remove(W))
 
-# W, T = single_payoff_matrix_vectors(strategy_one, strategy_two)
-# print(W[1:-1])
-# print(width_to_remove(W[1:-1]))
-# how many ways are the to put m rook in (i,j) chessboard with k_W in W and k_L in L, where W, T and L are described
-# by W and T
-
+#%%
 def is_single_type(W, T, j):
     if(np.min(W) == j):
+        return True
+    if(np.min(T) == j and np.max(W) == 0):
+        return True
+    if(np.max(T) == 0):
         return True
     return False
 
 def what_single_type(W, T, j):
+    print(W, T, j)
     if(W[-1] == j):
         return 1 ## type W
     if(T[-1] == j):
         return 0
     return -1
 
+def is_double_type_with_tie(W, T, j):
+    if(np.min(W) > 0 and np.min(W) == np.max(W) and np.min(T) == np.max(T) and T[-1] - W[-1] > 0): # T and W
+        return True
+    if(np.max(W) == 0 and np.max(T) > np.min(T)): # L and T
+        return True
+    return False
+
+def what_double_type(W, T, j):
+    if(np.max(W) == 0):
+        return -1
+    else:
+        return 1
+
+def vector_min(A, k):
+    for i in range(A.shape[0]):
+        if(A[i] > k):
+            A[i] = k
+    return A
+
 def H_0(i, j, m, k_W, k_L, flag):
-    # print("Wywołanie H_0")
+    # print("H0")
     # print(flag)
-    if(m > j or m > i):
-        # print("H_0, przypadek 1")
-        return 0
-    if(k_W + k_L > m):
-        # print("H_0, przypadek 2")
-        return 0
     if(flag == 1):
         if(k_L > 0):
             return 0
         if(m != k_W):
             return 0
-        # print("H_0 zwraca liczbę")
-        # print(k_W, m)
         return single_type_rectangle(i, j, m)
     if(flag == 0):
         if(k_L > 0):
@@ -189,68 +190,92 @@ def H_0(i, j, m, k_W, k_L, flag):
             return 0
         return single_type_rectangle(i, j, m)
 
+def H_1(i, j, m, k_W, k_L, W, T, flag):
+    if(min(k_L, k_W) != 0 ):
+        return 0
+    if(max(k_L, k_W) > m):
+        return 0
+    if(m > min(i,j)):
+        return 0
+    if(flag == -1):
+        width = width_to_remove(T)
+        return single_type_rectangle(i - width, j, k_L) * single_type_rectangle(width, j - k_L, m - k_L)
+    if(flag == 1):
+        height = T[-1] - W[-1]
+        return single_type_rectangle(i, j - height, k_W) * single_type_rectangle(i - k_W, height, m - k_W)
+
 
 def H(i, j, m, k_W, k_L, W, T):
-    # print("Wywołanie H")
-    if(is_single_type(W, T, j)):
-        # print("PRZYPADEK 1")
-        # print("ZWRACAM 0")
-        return H_0(i, j, m, k_W, k_L, what_single_type(W, T, j))
+    if(i == 0 or j == 0):
+        return 0
     if(m > j or m > i):
-        # print("PRZYPADEK 2")
-        # print("ZWRACAM 0")
         return 0
     if(k_W + k_L > m):
-        # print("PRZYPADEK 3")
-        # print("ZWRACAM 0")
         return 0
     if(max_rook_num(W) < k_W):
-        # print("PRZYPADEK 4")
-        # print("ZWRACAM 0")
         return 0
     if(max_rook_num(L_vector(W, T, j)) < k_L):
-        # print("PRZYPADEK 5")
-        # print("ZWRACAM 0")
-        # print(L_vector(W, T, j))
         return 0
+    if(is_single_type(W, T, j)):
+        return H_0(i, j, m, k_W, k_L, what_single_type(W, T, j))
     if(W[-1] == j): ## corner is in W
-        # print("PRZYPADEK 6")
+        # print("W CORNER")
         width = width_to_remove(W)
-        maximum_rooks_in_right = max(width, j)
+        maximum_rooks_in_right = min(width, j)
         sum = 0
-        # print("width:", width)
         for r in range(min(maximum_rooks_in_right, m, k_W) + 1):
-            # print(r)
             sum += H(i - width, j, m - r, k_W - r, k_L, W[:-width], T[:-width]) * single_type_rectangle(width, j - (m - r), r)
-        # print("ZWRACAM SUMĘ", sum)
         return sum
     if(T[-1] < j): ## corner is in L
-        # print("PRZYPADEK 7")
+        # print("L CORNER")
         height = j - T[-1]
-        maximum_rooks_in_top = max(i, height)
+        maximum_rooks_in_top = min(i, height)
         sum = 0
-        # print("height:", height)
         for r in range(min(maximum_rooks_in_top, m, k_L) + 1):
-            sum += H(i, j - height, m - r, k_W, k_L - r, W , T) * single_type_rectangle(i - (m - r), height, r)
-        # print("ZWRACAM SUMĘ", sum)
+            H_tmp = H(i, j - height, m - r, k_W, k_L - r, vector_min(W, j - height) , vector_min(T, j - height))
+            top = single_type_rectangle(i - (m - r), height, r)
+            sum += H_tmp * top
+            ## TODO czy nie powinniśmy zmniejszać W i T? done, bez zmian
         return sum
-    if(T[-1] == j):
-        # print("PRZYPADEK 8")
+    # REMISY
+    if(is_double_type_with_tie(W, T, j)):
+        return H_1(i, j, m, k_W, k_L, W, T, what_double_type(W, T, j))
+    if(T[-1] == j): ## corner is in T
+        # print("WESZLIŚMY W REMISY")
         width = width_to_remove(T)
         height = T[-1] - W[-1]
+        maximum_rooks_in_top = min(i - width, height)
+        maximum_rooks_in_right = min(width, j - height)
+        maximum_rooks_in_corner = min(width, height)
         sum = 0
+        for r_3 in range(min(maximum_rooks_in_top, m, k_L) + 1):
+            for r_2 in range(min(maximum_rooks_in_corner, m) + 1):
+                for r_1 in range(min(maximum_rooks_in_right, m, k_W) + 1):
+                    if(r_1 + r_2 + r_3 <= m):
+                        r_4 = m - r_1 - r_2 - r_3
+                        H_tmp = H(i - width, j - height, r_4, k_W - r_1, k_L - r_3, vector_min(W[:-width], j - height)
+                                  , vector_min(T[:-width], j - height))
+                        top = single_type_rectangle(i - width - r_4, height, r_3)
+                        corner = single_type_rectangle(width, height - r_3, r_2)
+                        right = single_type_rectangle(width - r_2, j - height - r_4, r_1)
+                        sum += H_tmp * top * corner * right
+            ## TODO czy nie powinniśmy zmniejszać W i T? done, bez zmian
+        return sum
         # print("WESZLIŚMY W REMISY")
 
     # trzy opcje, róg jest w W, L lub T
-
-# print(H(4, 4, 4, 2, 1, np.array([1,1,1,4]), np.array([1,1,1,4])))
+#%%
+print(list(range(1,2)))
+#%%
 print(H(5, 5, 5, 1, 4, np.array([1,1,4,4]), np.array([2,2,3,3])))
-# for i in range(6):
-#     for j in range(6):
-#         if(i + j == 5 or H(5, 5, 5, i, j, np.array([1,1,1,4,5]), np.array([1,1,1,4,5])) != 0):
-#             print("i", i, "j", j, "H", H(5, 5, 5, i, j, np.array([1,1,1,4,5]), np.array([1,1,1,4,5])))
+#%%
+A = np.array([2,3,4])
+B = np.array([2,4,3])
+# print(min(A, B))
+def vector_min(A, k):
+    for i in range(A.shape[0]):
+        if(A[i] > k):
+            A[i] = k
+    return A
 
-# print(width_to_remove(np.array([1,1,1,4])))
-# print(single_payoff_matrix(strategy_one, strategy_two))
-# print(single_type_rectangle(5,5,4))
-
+print(vector_min(A, 3))
--- symmetrized/payoff_matrix_tests.py
+++ symmetrized/payoff_matrix_tests.py
@@ -19,34 +19,14 @@ def H_results(strategy_one, strategy_two):
     fields_num = strategy_one.shape[0]
     # print(fields_num)
     W, T = single_payoff_matrix_vectors(strategy_one, strategy_two)
+    # print(W, T)
     # print(W)
-    res = np.zeros((fields_num, fields_num))
-    for i in range(fields_num):
-        for j in range(fields_num):
+    res = np.zeros((fields_num + 1, fields_num + 1))
+    for i in range(fields_num + 1):
+        for j in range(fields_num + 1):
             res[i,j] = H(fields_num, fields_num, fields_num, i, j, W, T)
     return res
-A = np.array([4,4,1,1])
-B = np.array([3,3,2,2])
-# print(H_results(A, B))
-# print(permutations_results(A, B))
-#%%
-A = np.array([2,1,1])
-B = np.array([4,0,0])
-print(H_results(A,B))
 
-#%%
-A_divides = divides(11, 4)
-B_divides = divides(10, 4)
-
-for A in A_divides:
-    for B in B_divides:
-        if(np.unique(A).shape[0] + np.unique(B).shape[0] == np.unique(np.append(A, B, axis=0)).shape[0]):
-            diff = H_results(A, B) - permutations_results(A, B)
-            if(np.max(diff) != 0 or np.min(diff) != 0):
-                print("BŁĄD dla ", A, B)
-                # print(diff)
-            else:
-                print("OK dla ", A, B)
 ## testowanie max_rook_num
 # def test_max_rook_num(startegy_one, strategy_two):
 
@@ -54,3 +34,37 @@ for A in A_divides:
 
 # print(payoff_from_perms_result(A, B))
 # print(symmetrized_pure_payoff_a(A, B))
+#
+def test(ties, print_errors):
+    A_divides = divides(15, 6)
+    B_divides = divides(16, 6)
+    num_errors = 0
+    num_tries = 0
+    error_A = []
+    error_B = []
+    for A in A_divides:
+        for B in B_divides:
+            if(ties or np.unique(A).shape[0] + np.unique(B).shape[0] == np.unique(np.append(A, B, axis=0)).shape[0]):
+                num_tries += 1
+                diff = H_results(A, B) - permutations_results(A, B)
+                if(np.max(diff) != 0 or np.min(diff) != 0):
+                    print("BŁĄD dla ", A, B)
+                    # print(diff)
+                    num_errors += 1
+                    error_A.append(A)
+                    error_B.append(B)
+                else:
+                    print("OK dla ", A, B)
+    print("Liczba błędów", num_errors)
+    print("Liczba prób", num_tries)
+    if(print_errors):
+        for i in range(len(error_A)):
+            print(error_A[i], error_B[i])
+
+test(True, False)
+#%%
+A = np.array([5,4,2])
+B = np.array([4,3,3])
+print(permutations_results(A, B))
+print(H_results(A, B))
+# print(H(2,2,2,1,1,np.array([1,1]),np.array([1,1])))
